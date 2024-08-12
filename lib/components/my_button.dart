a97f65b5341cec5cf5984c942d755f61fe99e505
Lucky Vishwakarma <110558983+LuckyVishwa1104@users.noreply.github.com>
2024-08-12 17:00:45 +0530

    added column for elevated button to cheive responsiveness

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -3,27 +3,36 @@ import 'package:flutter/material.dart';
 class MyButton extends StatelessWidget {
   final String buttonText;
   final VoidCallback onPressed;
-  const MyButton({super.key, required this.buttonText, required this.onPressed});
+  const MyButton(
+      {super.key, required this.buttonText, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ElevatedButton(
             onPressed: onPressed,
             style: ElevatedButton.styleFrom(
-              foregroundColor: Colors.white, 
-              backgroundColor: Colors.black, 
-              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20), // Text color
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.black,
+              padding: const EdgeInsets.symmetric(
+                  horizontal: 50, vertical: 20), // Text color
               textStyle: const TextStyle(
                 fontSize: 16,
               ),
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(8),
-                 // Rounded corners
+                // Rounded corners
               ),
             ).copyWith(
-              overlayColor: MaterialStateProperty.all(Colors.transparent)
-            ),
+                overlayColor: MaterialStateProperty.all(Colors.transparent)),
             child: Text(buttonText),
+          ),
+        ],
+      ),
     );
   }
 }
